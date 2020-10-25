60817f71d6637b4b665829b91fe87166b210619a
Weverson Nascimento <weverson.sn@gmail.com>
2020-10-25 18:05:48 -0300

    Add project fields to fill templates

--- bigbang/project.py
+++ bigbang/project.py
@@ -1,10 +1,10 @@
 import os
 import logging
-from typing import List
-from abc import abstractmethod
+from typing import List, Set
 from functools import reduce
 
 logger = logging.getLogger(__name__)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 class CreateFolderException(Exception):
@@ -17,6 +17,11 @@ class CreateTemplateException(Exception):
         super(CreateTemplateException, self).__init__(message)
 
 
+class MissingFieldException(Exception):
+    def __init__(self, message):
+        super(MissingFieldException, self).__init__(message)
+
+
 class Field:
 
     def __init__(self, name: str, value: str, default_value: any = None) -> None:
@@ -24,38 +29,32 @@ class Field:
         self.value = value
         self.default_value = default_value
 
+    def __eq__(self, o: object) -> bool:
+        return self.name == o.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __repr__(self) -> str:
+        return '{}({}, {}, {})'.format(__class__, self.name, self.value, self.default_value)
+
+    def __str__(self) -> str:
+        return self.name
+
 
 class Template:
-    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-    BASE_TEMPLATE_FOLDER = os.path.join(BASE_DIR, 'templates')
 
-    def __init__(self, from_path: str, to_path: str, fields: List[Field] = None) -> None:
-        self.from_path = os.path.join(self.BASE_TEMPLATE_FOLDER, from_path)
+    def __init__(self, from_path: str, to_path: str, fields: Set[Field] = None) -> None:
+        self.from_path = os.path.join(BASE_DIR, from_path)
         self.to_path = to_path
-        self.fields = fields
-        if self.fields == None:
-            self.fields = []
+        self.fields = fields if fields is not None else []
         self._content = None
-
-    @abstractmethod
-    def load(self):
-        raise NotImplementedError('The load method should be implement')
-
-    @abstractmethod
-    def create(self):
-        raise NotImplementedError('The create method should be implement')
-
-
-class LocalTemplate(Template):
-
-    def __init__(self, from_path: str, to_path: str, fields: List[Field] = None) -> None:
-        super().__init__(from_path, to_path, fields)
 
     def load(self):
         try:
             with open(self.from_path, 'r') as file:
                 self._content = file.read()
-            return self._load_fields()
+            return self._load_fields(self._content)
         except UnicodeDecodeError:
             message = 'Template file {} should be a valid utf-8 file'.format(
                 self.from_path)
@@ -66,18 +65,23 @@ class LocalTemplate(Template):
         try:
             with open(self.to_path, 'w') as file:
                 file.write(self.load())
-        except:
+        except OSError:
             message = 'Not was possible create template {}'.format(
                 self.to_path)
             logger.error(message, exc_info=True)
             raise CreateTemplateException(message)
 
-    def _load_fields(self):
-        fields = reduce(
-            lambda dict, field: {**dict, field.name: field.value},
-            self.fields, {}
-        )
-        return self._content.format(**fields)
+    def _load_fields(self, content):
+        try:
+            fields = reduce(
+                lambda dict, field: {**dict, field.name: field.value},
+                self.fields, {}
+            )
+            return content.format(**fields)
+        except KeyError as error:
+            message = 'Field {} not found'.format(error)
+            logger.error(message, exc_info=True)
+            raise MissingFieldException(message)
 
 
 class Folder:
@@ -115,11 +119,21 @@ class Folder:
 
 class Project:
 
-    def __init__(self, name: str, root_folder: Folder, folders: List[Folder], templates: List[Template]) -> None:
-        self.name = name
+    def __init__(
+        self,
+        root_folder: Folder,
+        templates: List[Template],
+        folders: List[Folder] = None,
+        fields: Set[Field] = None
+    ) -> None:
         self.root_folder = root_folder
-        self.folders = folders
-        self.templates = templates
+        self.folders = folders if folders is not None else []
+        self.templates = self._add_templates_to_project(templates)
+        self.fields = fields if fields is not None else []
+
+    @property
+    def name(self):
+        return self.root_folder.path.split('/')[-1]
 
     def create_folders(self):
         for folder in self.folders:
@@ -130,12 +144,19 @@ class Project:
 
     def create_templates(self):
         for template in self.templates:
+            template.fields = self.fields
             template.create()
 
         logger.info('{} templates created'.format(self.name))
 
+    def _add_templates_to_project(self, templates):
+        for template in templates:
+            template.to_path = os.path.join(
+                self.root_folder.path, template.to_path)
+        return templates
+
     def __repr__(self) -> str:
-        return '{}({}, {}, {}, {})'.format(__class__, self.name, self.root_folder, self.folders, self.templates)
+        return '{}({}, {}, {}, {})'.format(__class__,  self.root_folder, self.templates, self.folders, self.fields)
 
     def __str__(self) -> str:
         return self.name
